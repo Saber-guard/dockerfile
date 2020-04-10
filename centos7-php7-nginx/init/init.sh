8fb6894641916cbd532cb525e2b329ec343d6428
root <root@xieyi-d1.corp.qihoo.net>
2020-04-10 19:17:36 +0800

    modify

--- centos7-php7-nginx/init/init.sh
+++ centos7-php7-nginx/init/init.sh
@@ -7,14 +7,14 @@ mkdir -p /mnt/data/nginx
 chmod -R 777 /mnt/data
 
 #cron任务
-cp /mnt/init/crontab.conf /var/spool/cron/root
+cp /init/crontab.conf /var/spool/cron/root
 chmod 644 /var/spool/cron/root
 #nginx配置
-rm -rf /etc/nginx/conf.d
-ln -s /mnt/init/domain /etc/nginx/conf.d
+rm -f /etc/nginx/nginx.conf
+cp  /init/nginx.conf /etc/nginx/nginx.conf
 
 #php配置
 rm -f /etc/php.ini
-ln -s /mnt/init/php.ini /etc/php.ini
+cp  /init/php.ini /etc/php.ini
 rm -f /etc/php-fpm.conf
-ln -s /mnt/init/php-fpm.conf /etc/php-fpm.conf
+cp  /init/php-fpm.conf /etc/php-fpm.conf
